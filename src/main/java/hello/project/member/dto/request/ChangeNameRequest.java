333782c1ce340a01f4d254c501fe050fe00f91bb
pongchul <inchul7511@gmail.com>
2023-04-22 19:37:06 +0900

    이름 변경 요청 추가

---
+++ src/main/java/hello/project/member/dto/request/ChangeNameRequest.java
@@ -0,0 +1,16 @@
+package hello.project.member.dto.request;
+
+import com.sun.istack.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class ChangeNameRequest {
+
+    @NotNull
+    private String name;
+}
