51e680e3c88ef49864f99e275946d48bcb06109c
pongchul <inchul7511@gmail.com>
2023-04-23 16:51:27 +0900

    feature : 회원 에러 코드 추가

---
+++ src/main/java/hello/project/member/exception/MemberException.java
@@ -0,0 +1,10 @@
+package hello.project.member.exception;
+
+import hello.project.comm.exception.CustomException;
+
+public class MemberException extends CustomException {
+
+    public MemberException(MemberErrorCode message) {
+        super(message);
+    }
+}
