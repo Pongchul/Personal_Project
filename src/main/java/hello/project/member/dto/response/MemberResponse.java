5fabb6e4ab8cfce73913c1916091982fcc830c36
pongchul <inchul7511@gmail.com>
2023-04-27 12:41:25 +0900

    feature : 회원 dto 추가

---
+++ src/main/java/hello/project/member/dto/response/MemberResponse.java
@@ -0,0 +1,22 @@
+package hello.project.member.dto.response;
+
+import hello.project.member.domain.Member;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberResponse {
+
+    private Long id;
+    private String name;
+
+
+
+}
