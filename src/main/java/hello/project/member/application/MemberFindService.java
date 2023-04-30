b2d79fcdce888e5911f4f05fe0d8506fe0f9e8aa
pongchul <inchul7511@gmail.com>
2023-04-30 21:59:11 +0900

    feature : 회원 메소드 추가

---
+++ src/main/java/hello/project/member/application/MemberFindService.java
@@ -0,0 +1,39 @@
+package hello.project.member.application;
+
+import hello.project.member.domain.Member;
+import hello.project.member.domain.MemberRepository;
+import hello.project.member.domain.Password;
+import hello.project.member.domain.UserId;
+import hello.project.member.exception.MemberErrorCode;
+import hello.project.member.exception.MemberException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class MemberFindService {
+
+    private final MemberRepository memberRepository;
+
+    public Member findMember(Long id) {
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_EXIST));
+        validateExistMember(member);
+        return member;
+    }
+
+    public Member findByUserIdAndPassword(UserId userId, Password password) {
+        Member member = memberRepository.findByUserIdAndPassword(userId, password)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_INVALID_ID_AND_PASSWORD));
+        validateExistMember(member);
+        return member;
+    }
+
+    private void validateExistMember(Member member) {
+        if (member.isDeleted()) {
+            throw new MemberException(MemberErrorCode.MEMBER_DELETED);
+        }
+    }
+}
