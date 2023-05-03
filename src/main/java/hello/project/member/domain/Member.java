9579181229b8ccd36e3545251e476e5ff700ea9b
pongchul <inchul7511@gmail.com>
2023-05-03 12:23:55 +0900

    feature : 멤버 도메인 추가

--- src/main/java/hello/project/member/domain/Member.java
+++ src/main/java/hello/project/member/domain/Member.java
@@ -1,9 +1,11 @@
 package hello.project.member.domain;
 
+import hello.project.auth.support.PasswordEncoder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Optional;
 
 @Entity
 @Getter
@@ -17,6 +19,62 @@ public class Member {
 
     private String nickname;
 
+    @Embedded
+    private UserId userId;
+
+    @Embedded
+    private UserName userName;
+
+    @Embedded
+    private Password password;
+
+    @Column(nullable = false)
+    private boolean deleted;
 
 
+    public Member(UserId userId, Password password) {
+        this.userId = userId;
+        this.password = password;
+    }
+
+    public boolean isNotSamePassword(String password) {
+        return !this.password.isSame(password);
+    }
+
+    public void changePassword(String password, PasswordEncoder encoder) {
+        this.password = this.password.update(password, encoder);
+    }
+
+    public void changeUserName(String userName) {
+        this.userName = this.userName.update(userName);
+    }
+
+    public void delete() {
+        userId = null;
+        password = null;
+        userName = null;
+        deleted = true;
+    }
+
+    public boolean isSameUserId(Member member) {
+        return userId.getValue().equals(member.userId.getValue());
+    }
+
+    public String getUserId() {
+        return Optional.ofNullable(userId)
+                .map(UserId::getValue)
+                .orElse("");
+    }
+
+    public String getPassword() {
+        return Optional.ofNullable(password)
+                .map(Password::getValue)
+                .orElse("");
+    }
+
+    public String getUserName() {
+        return Optional.ofNullable(userName)
+                .map(UserName::getValue)
+                .orElse("");
+    }
 }
