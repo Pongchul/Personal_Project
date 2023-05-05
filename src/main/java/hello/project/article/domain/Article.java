8056f8f2ce4bf126be61447e7472465a471ab706
pongchul <inchul7511@gmail.com>
2023-05-05 12:59:03 +0900

    featuring : 게시글 도메인 설정

--- src/main/java/hello/project/article/domain/Article.java
+++ src/main/java/hello/project/article/domain/Article.java
@@ -1,5 +1,8 @@
 package hello.project.article.domain;
 
+import hello.project.article.domain.participant.Capacity;
+import hello.project.article.domain.participant.Participants;
+import hello.project.article.exception.ArticleException;
 import hello.project.comm.BaseDateTime;
 import hello.project.member.domain.Member;
 import lombok.AccessLevel;
@@ -8,6 +11,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+
+import static hello.project.article.exception.ArticleErrorCode.ALREADY_CLOSED_EARLY;
 
 @Entity
 @Getter
@@ -18,23 +23,28 @@ public class Article extends BaseDateTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
+    @Embedded
+    private Title title;
 
-    @Lob
-    private String contents;
+    @Embedded
+    private Participants participants;
 
-    private String destination;
+    @Embedded
+    private Contents contents;
 
-    private String location;
+    @Embedded
+    private Destination destination;
+
+    @Embedded
+    private Location location;
 
     @Enumerated(EnumType.STRING)
     private CurrentState currentState;
 
-    @ManyToOne
-    @JoinColumn(name = "member_id")
-    private Member author;
+    private boolean closedEarly;
 
-    public Article(Long id, String title, String contents, String destination, String location, CurrentState currentState, Member author,
+
+    public Article(Long id, Title title, Contents contents, Destination destination, Location location, CurrentState currentState, Member host,
                    LocalDateTime createdTime, LocalDateTime modifiedTime) {
         this.id = id;
         this.title = title;
@@ -42,8 +52,44 @@ public class Article extends BaseDateTime {
         this.destination = destination;
         this.location = location;
         this.currentState = currentState;
-        this.author = author;
         this.createdTime = createdTime;
         this.modifiedTime = modifiedTime;
     }
+
+    public void update(Capacity capacity, Contents contents, Location location, Title title, LocalDateTime modifiedTime, CurrentState currentState) {
+        validateGroupIsProceeding();
+        this.participants.updateCapacity(capacity);
+        this.contents = contents;
+        this.location = location;
+        this.title = title;
+        this.modifiedTime = modifiedTime;
+        this.currentState = currentState;
+    }
+
+    public void closeEarly() {
+        validateGroupIsProceeding();
+        closedEarly = true;
+    }
+
+    public void validateGroupIsProceeding() {
+        validateGroupIsNotClosedEarly();
+    }
+
+    public void validateGroupIsNotClosedEarly() {
+        if (closedEarly) {
+            throw new ArticleException(ALREADY_CLOSED_EARLY);
+        }
+    }
+
+    public boolean isHost(Member member) {
+        return participants.isHost(member);
+    }
+
+    public boolean isNotHost(Member member) {
+        return !isHost(member);
+    }
+
+
+
+
 }
