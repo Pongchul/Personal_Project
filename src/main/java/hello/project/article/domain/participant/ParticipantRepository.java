61ada837e21c329e02b1ce422e2fcca831452ec2
pongchul <inchul7511@gmail.com>
2023-05-09 11:02:02 +0900

    Featuring : 게시글 참가자 쿼리 추가

---
+++ src/main/java/hello/project/article/domain/participant/ParticipantRepository.java
@@ -0,0 +1,20 @@
+package hello.project.article.domain.participant;
+
+import hello.project.article.domain.Article;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ParticipantRepository extends Repository<Participant, Long> {
+
+    @Modifying
+    @Query("delete from Participant p where p.article.id = :articleId")
+    void deleteAllByArticleId(@Param("articleId") Long articleId);
+
+    @Modifying
+    @Query("delete from Participant p where p.member.Id = :memberId and p.article in (:articles)")
+    void deleteAllByMemberIdInArticles(@Param("memberId") Long memberId, @Param("articles") List<Article> articles);
+}
