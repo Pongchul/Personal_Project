bdab8fd31c4cb713ecac797a40fda511a5eb60ee
pongchul <inchul7511@gmail.com>
2023-05-13 13:47:43 +0900

    Featuring : 게시글 검색 DTO 추가

---
+++ src/main/java/hello/project/article/service/dto/request/ArticleSearchRequest.java
@@ -0,0 +1,23 @@
+package hello.project.article.service.dto.request;
+
+import hello.project.article.domain.Location;
+import hello.project.article.domain.search.SearchCondition;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class ArticleSearchRequest {
+
+    private int page = 0;
+    private String location;
+    private String destination;
+    private String CurrentState;
+    private String keyword;
+    private boolean alreadyClosed;
+
+    public SearchCondition toFindCondition() {
+        return new SearchCondition(location,keyword,CurrentState,alreadyClosed);
+    }
+}
