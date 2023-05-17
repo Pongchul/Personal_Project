d8410d99131d25d71c9aef6fff6e2dce7b8b74b4
pongchul <inchul7511@gmail.com>
2023-05-17 11:21:53 +0900

    Refactoring : 게시글 요청 DTO 위치, 목적지 추가

--- src/main/java/hello/project/article/service/dto/request/ArticleRequest.java
+++ src/main/java/hello/project/article/service/dto/request/ArticleRequest.java
@@ -1,8 +1,6 @@
 package hello.project.article.service.dto.request;
 
-import hello.project.article.domain.Contents;
-import hello.project.article.domain.CurrentState;
-import hello.project.article.domain.Title;
+import hello.project.article.domain.*;
 import hello.project.article.domain.participant.Capacity;
 import lombok.RequiredArgsConstructor;
 
@@ -12,12 +10,14 @@ public class ArticleRequest {
     private final String title;
     private final int capacity;
     private final String content;
+    private final LocationRequest location;
+    private final DestinationRequest destination;
+
 
 
     public Title getTitle() {
         return new Title(title);
     }
-
 
     public Capacity getCapacity() {
         return new Capacity(capacity);
@@ -26,5 +26,14 @@ public class ArticleRequest {
     public Contents getContents() {
         return new Contents(content);
     }
+
+    public Location getLocation() {
+        return location.getLocation();
+    }
+    public Destination getDestination() {
+        return destination.getLocation();
+    }
+
+
 
 }
