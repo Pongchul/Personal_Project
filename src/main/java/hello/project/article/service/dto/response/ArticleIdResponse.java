1dab0ab8f4602b29a311cc64ea2ad3db974fa6a8
pongchul <inchul7511@gmail.com>
2023-05-29 14:08:16 +0900

    featuring : 게시글 Service DTO 추가

---
+++ src/main/java/hello/project/article/service/dto/response/ArticleIdResponse.java
@@ -0,0 +1,11 @@
+package hello.project.article.service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ArticleIdResponse {
+
+    private Long articleId;
+}
