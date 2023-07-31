11ca0489b6b06ce9c0f3c917264f495da99be68a
pongchul <inchul7511@gmail.com>
2023-07-31 22:46:57 +0900

    Refactoring  : import 구문 정리

--- src/main/java/hello/project/article/domain/ArticleRepository.java
+++ src/main/java/hello/project/article/domain/ArticleRepository.java
@@ -1,10 +1,7 @@
 package hello.project.article.domain;
 
-import hello.project.member.domain.Member;
 import org.springframework.data.repository.Repository;
 
-import java.util.List;
-import java.util.Optional;
 
 public interface ArticleRepository extends Repository<Article, Long> {
 
