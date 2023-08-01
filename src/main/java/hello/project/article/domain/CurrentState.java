8fa973e3b64c732e2e1a40e5f4906afcce51bd41
pongchul <inchul7511@gmail.com>
2023-08-01 22:39:12 +0900

    Refactoring  : import 구문 정리

--- src/main/java/hello/project/article/domain/CurrentState.java
+++ src/main/java/hello/project/article/domain/CurrentState.java
@@ -2,20 +2,22 @@ package hello.project.article.domain;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+
 @RequiredArgsConstructor
 public enum CurrentState {
 
     RECRUITING("모집중"),
     COMPLETED("모집완료");
 
-    private final String currentState;
+    private final String currentStateName;
 
     public boolean isCompleted() {
         return this == COMPLETED;
     }
 
     public String getCurrentState() {
-        return currentState;
+        return currentStateName;
     }
 
 
