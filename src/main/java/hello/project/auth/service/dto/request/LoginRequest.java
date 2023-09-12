35aacd02dbf168f5260e22e979edc07e7f936f74
pongchul <inchul7511@gmail.com>
2023-09-12 21:01:15 +0900

    Featuring : Auth DTO Logic

---
+++ src/main/java/hello/project/auth/service/dto/request/LoginRequest.java
@@ -0,0 +1,19 @@
+package hello.project.auth.service.dto.request;
+
+import com.sun.istack.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class LoginRequest {
+
+    @NotNull
+    private String userId;
+
+    @NotNull
+    private String password;
+}
